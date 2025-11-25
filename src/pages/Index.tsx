8e62edc30f35fea355f79b6779b271eee5a407f1
samhoon000 <bdlsmhn@gmail.com>
2025-11-25 19:24:25 +0530

    Clean up outdated interface elements and simplify page layout

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import CareerTracks from "@/components/CareerTracks";
-import ProgressDashboard from "@/components/ProgressDashboard";
-import LearningPath from "@/components/LearningPath";
-import AIMentor from "@/components/AIMentor";
 import ResearchBacking from "@/components/ResearchBacking";
 import Footer from "@/components/Footer";
 
@@ -11,12 +8,9 @@ const Index = () => {
   return (
     <div className="min-h-screen">
       <Header />
-      <main>
+      <main className="space-y-24">
         <Hero />
         <CareerTracks />
-        <ProgressDashboard />
-        <LearningPath />
-        <AIMentor />
         <ResearchBacking />
       </main>
       <Footer />
